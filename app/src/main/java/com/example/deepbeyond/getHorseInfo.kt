e5113548ce9839a7708913e0b823cf8950a5ec52
textcunma <78415309+textcunma@users.noreply.github.com>
2023-10-27 10:19:01 +0900

    fix: remove for sentence, because contours.size=1

--- app/src/main/java/com/example/deepbeyond/getHorseInfo.kt
+++ app/src/main/java/com/example/deepbeyond/getHorseInfo.kt
@@ -45,38 +45,39 @@ fun getContourVertex(contours: List<MatOfPoint>, srcMat: Mat): Pair<MutableList<
     var conditionBboxX = 0  // 条件を満たす外接矩形のx座標
     var conditionBboxY = 0  // 条件を満たす外接矩形のy座標
     var conditionBboxH = 0  // 条件を満たす外接矩形の高さ
-    var conditionBboxW = 0  //
+    var conditionBboxW = 0  // 条件を満たす外接矩形の幅
 
+    // MatOfPointからMatOfPoint2fに変換
+    val contour = MatOfPoint2f(*contours[0].toArray())  // contours.size = 1
+
+    // 輪郭線の長さ
+    val arclen = Imgproc.arcLength(contour, true)
+
+    // 外接矩形計算
+    val boundingRect = Imgproc.boundingRect(contour)
+
+    // 外接矩形が画像サイズに対して比較的小さい場合はノイズとして無視(if判定でFalseとする)
     val approx = MatOfPoint2f()
-    for (contour in contours) {
-        // MatOfPointからMatOfPoint2fに変換
-        val contour = MatOfPoint2f(*contour.toArray())
+    if (h * scale < boundingRect.height && w * scale < boundingRect.width) {
+        // 輪郭線の近似
+        val epsilon = 0.005 * arclen
 
-        // 輪郭線の長さ
-        val arclen = Imgproc.arcLength(contour, true)
+        Imgproc.approxPolyDP(contour, approx, epsilon, true)
 
-        // 外接矩形計算
-        val boundingRect = Imgproc.boundingRect(contour)
-
-        // 外接矩形が画像サイズに対して比較的小さい場合はノイズとして無視(if判定でFalseとする)
-        if (h * scale < boundingRect.height && w * scale < boundingRect.width) {
-            // 輪郭線の近似
-            val epsilon = 0.005 * arclen
-
-            Imgproc.approxPolyDP(contour, approx, epsilon, true)
-
-            // 条件を満たす外接矩形の情報を記録
-            conditionBboxX = boundingRect.x
-            conditionBboxY = boundingRect.y
-            conditionBboxH = boundingRect.height
-            conditionBboxW = boundingRect.width
-        }
+        // 条件を満たす外接矩形の情報を記録
+        conditionBboxX = boundingRect.x
+        conditionBboxY = boundingRect.y
+        conditionBboxH = boundingRect.height
+        conditionBboxW = boundingRect.width
     }
 
+    // 近似輪郭の頂点を格納
     for (v in approx.toList()) {
         val pos = Pair(v.x, v.y)
         contourVertex.add(pos)
     }
+
+    // 画像内の馬に対する外接矩形の座標情報(4点)
     val bboxPosition = Rect(conditionBboxX, conditionBboxY, conditionBboxW, conditionBboxH)
     return Pair(contourVertex, bboxPosition)
 }
